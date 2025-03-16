5100932665045684e785f8fdb9fcefcd7db61751
MarounC6 <marounchahine@live.com>
2025-03-16 20:41:22 +0200

    main corrected

--- compiler/main.cpp
+++ compiler/main.cpp
@@ -9,48 +9,53 @@
 #include "generated/ifccBaseVisitor.h"
 
 #include "CodeGenVisitor.h"
+#include "VisitorAnalyseSyntaxique.h" // Include the header for VisitorAnalyseSyntaxique
 
 using namespace antlr4;
 using namespace std;
 
 int main(int argn, const char **argv)
 {
-  stringstream in;
-  if (argn==2)
-  {
-     ifstream lecture(argv[1]);
-     if( !lecture.good() )
-     {
-         cerr<<"error: cannot read file: " << argv[1] << endl ;
-         exit(1);
-     }
-     in << lecture.rdbuf();
-  }
-  else
-  {
-      cerr << "usage: ifcc path/to/file.c" << endl ;
-      exit(1);
-  }
-  
-  ANTLRInputStream input(in.str());
+    stringstream in;
+    if (argn==2)
+    {
+         ifstream lecture(argv[1]);
+         if( !lecture.good() )
+         {
+                 cerr<<"error: cannot read file: " << argv[1] << endl ;
+                 exit(1);
+         }
+         in << lecture.rdbuf();
+    }
+    else
+    {
+            cerr << "usage: ifcc path/to/file.c" << endl ;
+            exit(1);
+    }
+    
+    ANTLRInputStream input(in.str());
 
-  ifccLexer lexer(&input);
-  CommonTokenStream tokens(&lexer);
+    ifccLexer lexer(&input);
+    CommonTokenStream tokens(&lexer);
 
-  tokens.fill();
+    tokens.fill();
 
-  ifccParser parser(&tokens);
-  tree::ParseTree* tree = parser.axiom();
+    ifccParser parser(&tokens);
+    tree::ParseTree* tree = parser.axiom();
 
-  if(parser.getNumberOfSyntaxErrors() != 0)
-  {
-      cerr << "error: syntax error during parsing" << endl;
-      exit(1);
-  }
+    if(parser.getNumberOfSyntaxErrors() != 0)
+    {
+            cerr << "error: syntax error during parsing" << endl;
+            exit(1);
+    }
 
-  
-  CodeGenVisitor v;
-  v.visit(tree);
+    // Create an instance of VisitorAnalyseSyntaxique and visit the parse tree
+    VisitorAnalyseSyntaxique syntaxVisitor;
+    syntaxVisitor.visit(tree);
 
-  return 0;
+    // Create an instance of CodeGenVisitor and visit the parse tree
+    CodeGenVisitor codeGenVisitor;
+    codeGenVisitor.visit(tree);
+
+    return 0;
 }
